3a5413c840afe73e47c7937420c1cdcd59bde86b
oldpotter <oldpottertom@icloud.com>
2019-02-04 14:35:31 +0800

    首次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<view class="content">
+        <image class="logo" src="../../static/logo.png"></image>
+		<view>
+            <text class="title">{{title}}</text>
+        </view>
+		<button type="primary" @tap="tap">按钮</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		
+		onLoad() {
+
+		},
+		
+		methods: {
+			tap(){
+				uni.navigateTo({
+					url: '../HM-search/HM-search'
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
